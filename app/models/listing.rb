30774ca26df8c862fa9dc4d046a927d2f5bce28a
RUSSEL VIOLA <RUSSELVIOLA@GMAIL.COM>
2016-11-26 12:30:02 -0500

    added paperclip gem, image validations in listings model, added image file information to schema

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,7 @@
 class Listing < ApplicationRecord
+  has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "assets/images/default_ducky.jpg"
 
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   validates :name, presence: true
   validates :description, presence: true
   validates :price, numericality: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161115201848) do
+ActiveRecord::Schema.define(version: 20161126171341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "listings", force: :cascade do |t|
-    t.string   "name",        null: false
-    t.text     "description", null: false
-    t.decimal  "price",       null: false
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string   "name",               null: false
+    t.text     "description",        null: false
+    t.decimal  "price",              null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
