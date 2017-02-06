f8262ce8fe6e32caf1826aabb903e13d9faf5a20
RUSSEL VIOLA <RUSSELVIOLA@GMAIL.COM>
2017-02-06 15:51:48 -0500

    added sales and purchases tables

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,15 @@ Rails.application.routes.draw do
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'signin', to: 'sessions#new', as: 'signin'
 
-  get 'seller' => 'listings#seller'
+  get 'seller', to: 'listings#seller'
+  get 'sales', to: 'orders#sales'
+  get 'purchases', to: 'orders#purchases'
 
   root 'listings#index'
 
   resources :auth, only: :show
   resources :sessions, only: [:destroy, :new]
-  
+
   resources :listings do
     resources :orders
   end
